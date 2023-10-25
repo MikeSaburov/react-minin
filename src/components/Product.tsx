5cac3f6fe20d4a605a3b4b3b4fa14b2347968df3
mikhailir <saburov1985@gmail.com>
2023-10-25 23:54:39 +1100

    Добавил кнопку

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -10,7 +10,8 @@ export const Product = ({ product }: ProductProps) => {
       <img src={product.image} width={100} height={100} alt={product.title} />
       <p>{product.title}</p>
       <span className="font-bold">{product.price} &euro;</span>
-      <p>{product.description}</p>
+      <button className="py-2 px-4 border bg-yellow-400">Show Details</button>
+      {/* <p>{product.description}</p> */}
     </div>
   );
 };
