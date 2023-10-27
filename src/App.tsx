9729bfb16aeece37c1ee02fdb615360bfe166468
mikhailir <saburov1985@gmail.com>
2023-10-27 23:32:50 +1100

    fix добавил key в App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       {products.map((product) => (
-        <Product product={product} />
+        <Product key={product.id} product={product} />
       ))}
     </div>
   );
