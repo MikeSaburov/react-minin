5df86bbe6f67ed662bac534427c598f55d3a5554
mikhailir <saburov1985@gmail.com>
2023-10-27 23:03:44 +1100

    feature по клику на кнопку отобразить/скрыть описание товара в Product.tsx

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { IProduct } from '../models';
 
 interface ProductProps {
@@ -5,12 +6,20 @@ interface ProductProps {
 }
 
 export const Product = ({ product }: ProductProps) => {
+  const [description, setDiscription] = useState(false);
+
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
       <img src={product.image} width={100} height={100} alt={product.title} />
       <p>{product.title}</p>
       <span className="font-bold">{product.price} &euro;</span>
-      <button className="py-2 px-4 border bg-yellow-400">Show Details</button>
+      <button
+        className="py-2 px-4 border bg-yellow-400"
+        onClick={() => setDiscription(!description)}
+      >
+        Show Details
+      </button>
+      {description && <p>{product.description}</p>}
       {/* <p>{product.description}</p> */}
     </div>
   );
