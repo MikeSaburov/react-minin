40a92d1db91c41f4f3b7541ed695d270004c7bb5
mikhailir <saburov1985@gmail.com>
2023-10-27 23:16:50 +1100

    feature изменен класс и название кнопки в Product.tsx

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -8,16 +8,18 @@ interface ProductProps {
 export const Product = ({ product }: ProductProps) => {
   const [details, setDetails] = useState(false);
 
+  const btnClassName = details ? ' bg-yellow-400' : 'bg-blue-400';
+
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
       <img src={product.image} width={100} height={100} alt={product.title} />
       <p>{product.title}</p>
       <span className="font-bold">{product.price} &euro;</span>
       <button
-        className="py-2 px-4 border bg-yellow-400"
+        className={`py-2 px-4 border ${btnClassName}`}
         onClick={() => setDetails(!details)}
       >
-        Show Details
+        {details ? 'Hide Details' : 'Show Details'}
       </button>
       {details && <p>{product.description}</p>}
     </div>
