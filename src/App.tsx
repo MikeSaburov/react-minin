c009b5499862e5bb90d8ee85090d731cf1f2842d
mikhailir <saburov1985@gmail.com>
2023-10-27 23:31:42 +1100

    отображение продуктов с помощью map в App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import { products } from './data/Products';
 function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product product={products[0]} />
-      <Product product={products[1]} />
+      {products.map((product) => (
+        <Product product={product} />
+      ))}
     </div>
   );
 }
