254393a41c203979c7dfa945842b52f47641e26f
Mikhail Saburov <saburov1985@gmail.com>
2023-10-30 23:02:00 +1100

    feature(CreateProduct): создан компонент формы

---
+++ src/components/CreateProduct.tsx
@@ -0,0 +1,15 @@
+export const CreateProduct = () => {
+  return (
+    <form>
+      <input
+        type="text"
+        className="border py-2 px-4 mb-2"
+        placeholder="Enter product"
+      />
+
+      <button type="submit" className="py-2 px-4 border bg-yellow-400">
+        Create
+      </button>
+    </form>
+  );
+};
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import { CreateProduct } from './CreateProduct';
 
 export const Modal = () => {
   return (
     <>
       <div className="fixed bg-black/50 top-0 right-0 left-0 bottom-0" />
       <div className="w-[500px] p-5 rounded bg-white absolute top-10 left-1/2 -translate-x-1/2">
-        <h1>Modal</h1>
+        <CreateProduct />
       </div>
     </>
   );
